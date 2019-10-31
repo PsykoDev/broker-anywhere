033df291e3119c1942ed449b04fde60d56a9606e
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2019-10-31 05:50:41 +0100

    Auto update & Update 86

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-module.exports = function BrokerAnywhere(dispatch){
-    const chatHook = event => {
-        if(!event.message.toLowerCase().includes('!broker')) return
-        
-        dispatch.toClient('S_NPC_MENU_SELECT', 1, {type:28})
-        return false
-    }
-    dispatch.hook('C_CHAT', 1, chatHook)
-    dispatch.hook('C_WHISPER', 1, chatHook)
-}
+	module.exports = function TradeAnywhere(mod){
+    
+		mod.command.add('broker', () => {
+		mod.send('S_NPC_MENU_SELECT', 1 , {type:28})
+		});
+		
+		mod.command.add('fishchest', () => { 
+		mod.send('S_NPC_MENU_SELECT', 1 , {type:1001})
+		});
+		
+		mod.command.add('fishboy', () => {
+        mod.send('S_NPC_MENU_SELECT', 1 , {type:1000})
+		});	
+	
+		mod.command.add('signumtrader', () => {
+        mod.send('S_NPC_MENU_SELECT', 1 , {type:16095})
+		});	
+	
+		mod.command.add('trader', () => {
+        mod.send('S_NPC_MENU_SELECT', 1 , {type:16062})
+		});
+	
+		mod.command.add('fishtrader', () => {
+        mod.send('S_NPC_MENU_SELECT', 1 , {type:16094})	
+		});
+	
+		mod.command.add('alchi', () => {
+		mod.send('S_NPC_MENU_SELECT', 1 , {type:193})
+		});
+	
+		mod.command.add('warehouse', () => {
+        mod.send('S_NPC_MENU_SELECT', 1 , {type:4})	
+		}); 
+	
+};
+
+
